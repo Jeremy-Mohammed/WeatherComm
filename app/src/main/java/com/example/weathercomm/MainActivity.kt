980e93f9ec63f5ed49b456e28283f30eb5f7d90f
Jeremy Mohammed <59875684+Jeremy-Mohammed@users.noreply.github.com>
2023-01-23 01:54:05 -0500

    Update MainActivity.kt
    Upload API key

--- app/src/main/java/com/example/weathercomm/MainActivity.kt
+++ app/src/main/java/com/example/weathercomm/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val city: String = "toronto,ca"
+    val API: String = "b0fa7e6d6ef32271d13bc15b0a485688"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
