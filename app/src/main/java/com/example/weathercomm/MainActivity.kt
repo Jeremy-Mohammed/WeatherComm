90d9a52d1d1fd059ff798786480809599141ca78
Jeremy Mohammed <59875684+Jeremy-Mohammed@users.noreply.github.com>
2023-01-25 23:27:42 -0500

    Update MainActivity.kt
    Finished adding text fields, fixed type bugs

--- app/src/main/java/com/example/weathercomm/MainActivity.kt
+++ app/src/main/java/com/example/weathercomm/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.weathercomm
 
 import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
@@ -10,6 +11,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,10 +35,10 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        override fun doInBackground(vararg p0: String?): String {
+        override fun doInBackground(vararg p0: String?): String? {
             var response:String?
             try{
-                response = URL(spec:"https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(Charsets.UTF_8)
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(Charsets.UTF_8)
             }
             catch (e: Exception)
             {
@@ -45,12 +48,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onPostExecute(result: String?){
-            super.onPostExwecute(result)
+            super.onPostExecute(result)
             try{
                 val jsonObj = JSONObject(result)
-                val main = jsonObj.getJSONObject(name: "main")
-                val sys = jsonObj.getJSONObject(name: "sys")
-                val wind = jsonObj.getJSONObject(name: "wind")
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val updatedAt:Long = jsonObj.getLong("dt")
+                val updatedAtText = "Update at:" + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp = main.getString("temp") + "°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min") + "°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max") + "°C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+                val sunrise:Long = sys.getLong("sunrise")
+                val sunset:Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+                val address = jsonObj.getString("name") + ", " + sys.getString("country")
+
+                
             }
         }
     }
